15bb055730ed11ff1f640c426fe74a00ef04daae
richard-liang <20082751@bit.edu.cn>
2014-10-29 16:24:58 +0800

    some update

--- lars.py
+++ lars.py
@@ -38,22 +38,50 @@ def lars(xs, ys):
     print regularXs
     print regularYs
     print regularYs.shape
+    AFullSet = set(range(0,P)) 
+    ASet = set([]) 
+    AcSet = set(range(0,P))
     UaHat = np.asmatrix([[0] for i in range(N)])
-    corr = np.transpose(regularXs)*(regularYs-UaHat)
-    absCorr = np.absolute(corr)
-    print corr
-    (maxCorrIdx,_) = np.unravel_index(absCorr.argmax(), absCorr.shape) 
-    sj = 1
-    if corr[maxCorrIdx] < 0:
-        sj = -1
-    Xa = np.asmatrix([]).reshape(N,0)
-    Xa = np.append(Xa, sj*regularXs[:,maxCorrIdx], axis=1)
-    XaTXaInv = alg.inv(np.transpose(Xa)*Xa)
-    (_,p) = Xa.shape
-    Ia = np.matrix([[1] for i in range(p)])
-    Aa = np.power(Ia*XaTXaInv*Ia, -0.5)
-    Ua = Xa*XaTXaInv*Aa*Ia
 
-#xs, ys = input()
-#lars(xs, ys)
-lars(np.array([[101,157,4,87],[87,183,3,69],[93,156,4,85]]), np.array([[151],[75],[141]]))
+    for step in range(0,P):
+        corr = np.transpose(regularXs)*(regularYs-UaHat)
+        absCorr = np.absolute(corr)
+        cHat = max(absCorr)
+    #    print corr
+        maxCorrIdx = 0
+        maxCorr = -2.0
+        for ci in AcSet:
+            if absCorr[ci] > maxCorr:
+                maxCorr = absCorr[ci]
+                maxCorrIdx = ci
+#        (maxCorrIdx,_) = np.unravel_index(absCorr.argmax(), absCorr.shape) 
+        ASet.add(maxCorrIdx)
+        print "add", maxCorrIdx +1
+        sj = 1
+        if corr[maxCorrIdx] < 0:
+            sj = -1
+        Xa = np.asmatrix([]).reshape(N,0)
+        Xa = np.append(Xa, sj*regularXs[:,maxCorrIdx], axis=1)
+        XaTXaInv = alg.inv(np.transpose(Xa)*Xa)
+        (_,p) = Xa.shape
+        Ia = np.matrix([[1] for i in range(p)])
+        Aa = np.power(Ia*XaTXaInv*Ia, -0.5)
+        Ua = Xa*XaTXaInv*Aa*Ia
+        AcSet = AFullSet - ASet
+        a = np.transpose(regularXs)*Ua
+        GamaHat = 0
+        minj = None 
+        for j in AcSet:
+            val = (cHat-corr[j])/(Aa-a[j,0])
+            if val > 0 and (minj == None or val < GamaHat):
+                GamaHat = val[0,0]
+                minj = j
+            val = (cHat+corr[j])/(Aa+a[j,0])
+            if val > 0 and (minj == None or val < GamaHat):
+                GamaHat = val[0,0]
+                minj = j
+        UaHat = UaHat+GamaHat*Ua
+     
+xs, ys = input()
+lars(xs, ys)
+#lars(np.array([[101,157,4,87],[87,183,3,69],[93,156,4,85]]), np.array([[151],[75],[141]]))
