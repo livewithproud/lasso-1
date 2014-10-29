4a6615731f4080893dbf61c4b4ceb6dbc12ca828
richard-liang <20082751@bit.edu.cn>
2014-10-29 15:08:40 +0800

    some update

--- lars.py
+++ lars.py
@@ -1,4 +1,5 @@
 import numpy as np
+import numpy.linalg as alg
 
 def input():
     rawdata = []
@@ -27,24 +28,32 @@ def lars(xs, ys):
     varXs = np.var(xs, axis=0)
     regularXs = np.asmatrix(np.divide(np.subtract(xs, meanXs), np.sqrt(N*varXs)))
 #    print np.mean(regularXs, axis=0)
-#    print np.linalg.norm(regularXs, axis=0)
+#    print alg.norm(regularXs, axis=0)
     print regularXs.shape
     meanYs = np.mean(ys)
     varYs = np.var(ys)
     regularYs = np.asmatrix(np.divide(np.subtract(ys, meanYs), np.sqrt(N*varYs)))
 #    print np.mean(regularYs, axis=0)
-#    print np.linalg.norm(regularYs, axis=0)
+#    print alg.norm(regularYs, axis=0)
+    print regularXs
+    print regularYs
     print regularYs.shape
-    corr = np.transpose(regularXs)*regularYs
+    UaHat = np.asmatrix([[0] for i in range(N)])
+    corr = np.transpose(regularXs)*(regularYs-UaHat)
     absCorr = np.absolute(corr)
     print corr
     (maxCorrIdx,_) = np.unravel_index(absCorr.argmax(), absCorr.shape) 
     sj = 1
     if corr[maxCorrIdx] < 0:
         sj = -1
-    Xa = regularXs[:,maxCorrIdx]
-     
+    Xa = np.asmatrix([]).reshape(N,0)
+    Xa = np.append(Xa, sj*regularXs[:,maxCorrIdx], axis=1)
+    XaTXaInv = alg.inv(np.transpose(Xa)*Xa)
+    (_,p) = Xa.shape
+    Ia = np.matrix([[1] for i in range(p)])
+    Aa = np.power(Ia*XaTXaInv*Ia, -0.5)
+    Ua = Xa*XaTXaInv*Aa*Ia
 
-xs, ys = input()
-lars(xs, ys)
-#lars([[1,2,3],[4,5,6]], [1,1])
+#xs, ys = input()
+#lars(xs, ys)
+lars(np.array([[101,157,4,87],[87,183,3,69],[93,156,4,85]]), np.array([[151],[75],[141]]))
